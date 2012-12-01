fb116d25f4642d1d1538b0e19272d463c923e1db
Robert Murray McMahon <Robert.Murray.McMahon@gmail.com>
2012-12-01 03:46:15 -0600

    Forgot to comment out.

--- src/tmux-powerline/segments/np_cmus.sh
+++ src/tmux-powerline/segments/np_cmus.sh
@@ -3,7 +3,7 @@
 cmus_pid=$(ps -A | grep -m1 cmus | awk '{print $1}')
 if [ -n "$cmus_pid" ]; then
     status=$(cmus-remote -Q | grep "status" | cut -d ' ' -f 2)
-    artist=$(cmus-remote -Q | grep -m 1 "artist" | cut -d ' ' -f 3-)
+    #artist=$(cmus-remote -Q | grep -m 1 "artist" | cut -d ' ' -f 3-)
     title=$(cmus-remote -Q | grep "title" | cut -d ' ' -f 3-)
     track=$(cmus-remote -Q | grep "tracknumber" | cut -d ' ' -f 3)
 
